835f07d8f3960ee9a2eb11b3f37ed5e2569ad98d
[vikatgen] <[vikat.gen@gmail.com]>
2020-05-06 15:05:25 +0300

    Add: Tailwind colors and fonts

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="w-screen h-full bg-yellow-dark">
+    <div class="header-background"></div>
+    <div class="header-absolute-flowers"></div>
+    <div class="header-absolute-leaves"></div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
