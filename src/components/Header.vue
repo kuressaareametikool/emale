223495d7e96d14d2840b9b56129362605b66fcc2
[vikatgen] <[vikat.gen@gmail.com]>
2020-05-06 15:29:45 +0300

    Remove: Addedd tailwind.css to .gitignore

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,5 @@
 <template>
-  <section class="w-screen h-full bg-yellow-dark">
-    <div class="header-background"></div>
-    <div class="header-absolute-flowers"></div>
-    <div class="header-absolute-leaves"></div>
-  </section>
+  <section class="w-screen h-full bg-yellow-dark"></section>
 </template>
 
 <script>
