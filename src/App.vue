b48d8edeb6645a2dde469fea17ff5d6f009fa018
[vikatgen] <[vikat.gen@gmail.com]>
2020-05-07 16:37:59 +0300

    Add: Concern section

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app" class="font-serif">
     <Header />
     <Motivation />
+    <Concern />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import Motivation from "./components/Motivation.vue";
+import Concern from "./components/Concern.vue";
 
 export default {
   name: "App",
   components: {
     Header,
-    Motivation
+    Motivation,
+    Concern
   }
 };
 </script>
---
+++ src/components/Concern.vue
@@ -0,0 +1,62 @@
+<template>
+  <section id="concern-section" class="mt-20 font-sans font-light text-brown">
+    <div class="mb-16">
+      <h1 class="font-serif text-center text-xl">Meie mure.</h1>
+      <img class="mx-auto" src="../assets/heading-line.svg" alt="Line under heading" />
+    </div>
+    <div class="container">
+      <div class="lg:flex">
+        <!-- Concern content left -->
+        <div
+          class="mb-8 w-full bg-yellow-light p-8 rounded-lg sm:max-w-lg sm:mx-auto lg:w-1/2 lg:text-xl lg:w-1/2 lg:mr-2"
+        >
+          <p
+            class="mb-8 tracking-wide leading-6 sm:text-center lg:text-left leading-6 lg:text-left"
+          >Siiski on Eestil palju veel areneda, et ka sisuliselt toetada naisi ja emasid.</p>
+          <p
+            class="mb-8 tracking-wide leading-6 sm:text-center lg:text-left leading-6"
+          >Selleks on vaja hakata hindama naiste mitterahalist panust.</p>
+          <p
+            class="tracking-wide leading-6 sm:text-center lg:text-left leading-6"
+          >Selleks on vaja märgata võimalike kitsaskohti seaduandluses, et emad oleks kaitstud rahaliste ja juriidiliste raskuste eest.</p>
+        </div>
+        <!-- Concern content right -->
+        <div
+          class="mb-8 w-full bg-yellow-light p-8 rounded-lg sm:max-w-lg sm:mx-auto lg:w-1/2 lg:text-xl lg:ml-2"
+        >
+          <p
+            class="mb-8 tracking-wide leading-6 sm:text-center lg:text-left leading-6"
+          >Oluline on pakkuda vajalikku infot ja tuge, et leida lahendusi läbi keeruliste olukordade, mis puudutavad koduvägivalda.</p>
+          <p
+            class="mb-8 tracking-wide leading-6 sm:text-center lg:text-left leading-6"
+          >Palju on veel teha, et tõesti näidata emadele, et nende panust hinnatakse, märgatakse ja peetakse oluliseks.</p>
+          <p
+            class="mb-8 tracking-wide leading-6 sm:text-center lg:text-left leading-6"
+          >Ainult sõnadest ei piisa.</p>
+          <p class="tracking-wide leading-6 sm:text-center lg:text-left leading-6">Vaja on tegusid.</p>
+        </div>
+      </div>
+      <!-- Concern section image -->
+      <div class="mb-8 flex justify-end">
+        <img src="../assets/header-background-leaves.svg" alt="Concern section image" />
+      </div>
+      <!-- Scroll-down indicator -->
+      <div class>
+        <img class="mx-auto" src="../assets/arrow_down.svg" alt="Scroll down indicator" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.container {
+  width: 90%;
+  max-width: 1088px;
+  margin: auto;
+  height: 100%;
+}
+</style>
