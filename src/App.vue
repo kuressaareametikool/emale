03efbdcc4798b258f430183588c828b273775738
[vikatgen] <[vikat.gen@gmail.com]>
2020-05-07 16:10:47 +0300

    Add: Motivation section

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app" class="font-serif">
     <Header />
+    <Motivation />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Motivation from "./components/Motivation.vue";
 
 export default {
   name: "App",
   components: {
-    Header
+    Header,
+    Motivation
   }
 };
 </script>
